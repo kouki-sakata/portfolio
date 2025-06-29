2906e70e3ce118bb4fc53130b81e22ac50c3813d
kouki-sakata <kouki.sakata.works@gmail.com>
2025-06-29 14:29:29 +0900

    refactor: 不要なemployee_list.jsファイルを削除

--- src/main/resources/static/js/employee_list.js
+++
@@ -1,6 +0,0 @@
-//一般/管理者のトグルボタン変更
-$("input[name='tbody_toggle']").change(function(){
-    let selectedTbody = $(this).val();
-    $("tbody").hide(); // すべてのtbodyを非表示にする
-    $("#" + selectedTbody).show(); // 選択されたtbodyを表示する
-});
