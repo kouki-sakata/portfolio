137966e9ba9d16ee9c92b1576716ee73d463afca
kouki-sakata <kouki.sakata.works@gmail.com>
2025-06-29 17:43:15 +0900

    feat: HTMLにCSSを適用

--- src/main/java/com/example/teamdev/service/StampOutputService01.java
+++ src/main/java/com/example/teamdev/service/StampOutputService01.java
@@ -12,6 +12,8 @@ import java.util.Map;
 
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
