02d7a38e2fb22e0c1f81fc26bd24bba97300713d
kouki-sakata <kouki.sakata.works@gmail.com>
2025-06-30 21:03:13 +0900

    Fix: ルートURLでnullエラーが発生する問題を修正

--- src/main/java/com/example/teamdev/controller/SignInController.java
+++ src/main/java/com/example/teamdev/controller/SignInController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/signin")
+@RequestMapping({"/", "/signin"})
 public class SignInController {
 
     private static final Logger logger = LoggerFactory.getLogger(SignInController.class);
