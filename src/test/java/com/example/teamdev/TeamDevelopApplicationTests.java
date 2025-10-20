e35fce608b947232a6c03b549f4a4bb0b52baf33
kouki-sakata <kouki.sakata.works@gmail.com>
2025-10-20 18:18:47 +0900

    fix: testing

--- src/test/java/com/example/teamdev/TeamDevelopApplicationTests.java
+++ src/test/java/com/example/teamdev/TeamDevelopApplicationTests.java
@@ -1,16 +1,16 @@
 package com.example.teamdev;
 
+import com.example.teamdev.testconfig.PostgresContainerSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import com.example.teamdev.testconfig.PostgresContainerSupport;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class TeamDevelopApplicationTests extends PostgresContainerSupport {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Application context should start without issues.
+    }
 }
